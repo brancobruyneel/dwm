d821056de207fa7734b05df536f2d6c84aab780b
Branco <branco.bruyneel@gmail.com>
2020-12-30 18:09:59 +0100

    Add icons to tags

--- config.def.h
+++ config.def.h
@@ -35,7 +35,7 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
