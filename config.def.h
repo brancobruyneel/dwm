07df9f40b4a3385f796a41fe86f42c296168ac65
Branco <branco.bruyneel@gmail.com>
2020-12-31 17:48:51 +0100

    Back to st

--- config.def.h
+++ config.def.h
@@ -89,7 +89,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 #include <X11/XF86keysym.h>
 #include "movestack.c"
