b0ac9fed51fa8a5ff464b1e0b4c279ef9dc576ba
Branco <branco.bruyneel@gmail.com>
2021-03-21 15:54:44 +0100

    Update rules

--- config.def.h
+++ config.def.h
@@ -42,13 +42,14 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class        instance        title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "alacritty",  				NULL,           NULL,           0,         0,          1,           0,        -1 },
+	{ "alacritty",  NULL,           NULL,           0,         0,          1,           0,        -1 },
 	{ "firefox",    NULL,           NULL,           1 << 1,    0,          0,          -1,        -1 },
 	{ "Postman",    NULL,           NULL,           1 << 2,    0,          0,           0,        -1 },
 	{ "Gimp",       NULL,           NULL,           1 << 2,    0,          0,           0,        -1 },
 	{ "discord",    NULL,           NULL,           1 << 4,    0,          0,           0,        -1 },
 	{ "Pavucontrol",NULL,           NULL,						0,				 1,          0,           0,        -1 },
 	{ "matplotlib", NULL,           NULL,						0,				 1,          0,           0,        -1 },
+	{ "Tk",         NULL,           NULL,						0,				 1,          0,           0,        -1 },
 	{ NULL,         NULL,           "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
