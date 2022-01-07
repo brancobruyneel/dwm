11b82dc9dc2b6a8e38005de124a368f142fb52e0
Branco <branco.bruyneel@gmail.com>
2022-01-07 15:41:29 +0000

    style(config)

--- config.def.h
+++ config.def.h
@@ -13,7 +13,7 @@ static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static const char dmenufont[]       = "monospace:size=10";
 static const char *fonts[]          = { 
-    "Ubuntu Mono:style=bold:size=12",
+    "Hack:style=Bold:size=10",
     "Font Awesome 5 Brands:size=10",
     "Font Awesome 5 Free Solid:size=10",
     "Font Awesome 5 Free Regular:size=10",
@@ -32,18 +32,25 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class        instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",       NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox",    NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "Alacritty",  NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,         NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	/* class                        instance        title               tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "st",                         NULL,           NULL,               0,         0,          1,           0,        -1 },
+	{ "firefox",                    NULL,           NULL,               1 << 1,    0,          0,          -1,        -1 },
+	{ "Postman",                    NULL,           NULL,               1 << 2,    0,          0,           0,        -1 },
+	{ "Gimp",                       NULL,           NULL,               1 << 2,    0,          0,           0,        -1 },
+	{ "discord",                    NULL,           NULL,               1 << 4,    0,          0,           0,        -1 },
+	{ "Pavucontrol",                NULL,           NULL,		        0,		   1,          0,           0,        -1 },
+	{ "matplotlib",                 NULL,           NULL,		        0,		   1,          0,           0,        -1 },
+	{ "Tk",                         NULL,           NULL,		        0,		   1,          0,           0,        -1 },
+	{ "microsoft teams - preview",  NULL,           NULL,		        1 << 3,	   1,          0,           0,        -1 },
+	{ "libreoffice",                NULL,           NULL,		        1 << 3,	   1,          0,           0,        -1 },
+	{ NULL,                         NULL,           "Event Tester",     0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
@@ -84,7 +91,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 
 /*
@@ -165,22 +172,29 @@ static Key keys[] = {
     { 0, XF86XK_MonBrightnessUp,	spawn,		SHCMD("xbacklight -inc 15") },
 	{ 0, XF86XK_MonBrightnessDown,	spawn,		SHCMD("xbacklight -dec 15") },
 
-	/* { MODKEY|Mod4Mask,              XK_u,      incrgaps,       {.i = +1 } }, */
-	/* { MODKEY|Mod4Mask|ShiftMask,    XK_u,      incrgaps,       {.i = -1 } }, */
-	/* { MODKEY|Mod4Mask,              XK_i,      incrigaps,      {.i = +1 } }, */
-	/* { MODKEY|Mod4Mask|ShiftMask,    XK_i,      incrigaps,      {.i = -1 } }, */
-	/* { MODKEY|Mod4Mask,              XK_o,      incrogaps,      {.i = +1 } }, */
-	/* { MODKEY|Mod4Mask|ShiftMask,    XK_o,      incrogaps,      {.i = -1 } }, */
-	/* { MODKEY|Mod4Mask,              XK_6,      incrihgaps,     {.i = +1 } }, */
-	/* { MODKEY|Mod4Mask|ShiftMask,    XK_6,      incrihgaps,     {.i = -1 } }, */
-	/* { MODKEY|Mod4Mask,              XK_7,      incrivgaps,     {.i = +1 } }, */
-	/* { MODKEY|Mod4Mask|ShiftMask,    XK_7,      incrivgaps,     {.i = -1 } }, */
-	/* { MODKEY|Mod4Mask,              XK_8,      incrohgaps,     {.i = +1 } }, */
-	/* { MODKEY|Mod4Mask|ShiftMask,    XK_8,      incrohgaps,     {.i = -1 } }, */
-	/* { MODKEY|Mod4Mask,              XK_9,      incrovgaps,     {.i = +1 } }, */
-	/* { MODKEY|Mod4Mask|ShiftMask,    XK_9,      incrovgaps,     {.i = -1 } }, */
-	/* { MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} }, */
-	/* { MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} }, */
+
+  /* utils */
+	{ MODKEY|ShiftMask,             XK_s,      spawn,	       SHCMD("flameshot gui") },
+	{ MODKEY,			            XK_w, 	   spawn,	       SHCMD("$BROWSER") },
+    { MODKEY,                       XK_F1,     spawn,          SHCMD("feh --bg-scale --randomize ~/Pictures/wallpapers/*") },
+    { MODKEY|ShiftMask,             XK_p,      spawn,          SHCMD("rofi-rbw -a copy-password -r ''") },
+
+	{ MODKEY|Mod4Mask,              XK_u,      incrgaps,       {.i = +1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_u,      incrgaps,       {.i = -1 } },
+	{ MODKEY|Mod4Mask,              XK_i,      incrigaps,      {.i = +1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_i,      incrigaps,      {.i = -1 } },
+	{ MODKEY|Mod4Mask,              XK_o,      incrogaps,      {.i = +1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_o,      incrogaps,      {.i = -1 } },
+	{ MODKEY|Mod4Mask,              XK_6,      incrihgaps,     {.i = +1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_6,      incrihgaps,     {.i = -1 } },
+	{ MODKEY|Mod4Mask,              XK_7,      incrivgaps,     {.i = +1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_7,      incrivgaps,     {.i = -1 } },
+	{ MODKEY|Mod4Mask,              XK_8,      incrohgaps,     {.i = +1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_8,      incrohgaps,     {.i = -1 } },
+	{ MODKEY|Mod4Mask,              XK_9,      incrovgaps,     {.i = +1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_9,      incrovgaps,     {.i = -1 } },
+	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
 
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
